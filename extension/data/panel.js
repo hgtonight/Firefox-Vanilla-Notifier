e061f2004eb6a75483a27e878afc8700d78bef76
Zachary Doll <hgtonight@gmail.com>
2015-01-30 10:42:38 -0600

    Only search for the nodes once.

--- extension/data/panel.js
+++ extension/data/panel.js
@@ -1,40 +1,49 @@
+var forumName = document.getElementById("forumName");
+var rootInput = document.getElementById("forumRoot");
+var forumEntryAnchor = document.getElementById("forumEntry");
+var notificationList = document.getElementById("notificationList");
+var infoBox = document.getElementById("infoBox");
+var throbber = document.getElementById("throbber");
+var errorMessage = document.getElementById("errorMessage");
+
 attachClickEvents(document.getElementsByTagName("a"));
 
 self.port.on("send-list", function(listHtml) {
-    document.getElementById("notificationList").innerHTML = listHtml;
-    attachClickEvents(document.getElementById("notificationList").getElementsByTagName('a'));
+    notificationList.innerHTML = listHtml;
+    attachClickEvents(notificationList.getElementsByTagName('a'));
 });
 
-self.port.on("send-storage", function(forumRoot, forumName) {
-    document.getElementById("forumName").textContent = forumName;
-    document.getElementById("forumEntry").href = forumRoot + "/entry/signin";
-    document.getElementById("forumRoot").value = forumRoot;
-    document.getElementById("throbber").className = "Hidden";
+self.port.on("send-storage", function(urlRoot, siteName) {
+    rootInput.value = urlRoot;
+    forumName.textContent = siteName;
+    forumEntryAnchor.href = urlRoot + "/entry/signin";
+    throbber.className = "Hidden";
 });
 
 /* hide the notification list if logged out */
 self.port.on("loggedOut", function() {
-    document.getElementById("notificationList").className = "Hidden";
-    document.getElementById("infoBox").className = "";
+    notificationList.className = "Hidden";
+    infoBox.className = "";
 });
 
 /* hide the settings if logged in */
 self.port.on("loggedIn", function() {
-    document.getElementById("infoBox").className = "Hidden";
-    document.getElementById("notificationList").className = "";
+    infoBox.className = "Hidden";
+    notificationList.className = "";
 });
 
 /* show an error message if an invalid url is entered */
-self.port.on("invalidVanillaForum", function() {
-   document.getElementById("errorMessage").className = ""; 
-   document.getElementById("throbber").className = "Hidden";
+self.port.on("invalidVanillaForum", function(msg) {
+   errorMessage.className = ""; 
+   errorMessage.innerHTML = msg;
+   throbber.className = "Hidden";
 });
 
 /* Update the stored webroot when changed */
-document.getElementById("forumRoot").addEventListener("change", function(e) {
+rootInput.addEventListener("change", function(e) {
     self.port.emit("updatedForumRoot", this.value);
-    document.getElementById("errorMessage").className = "Hidden";
-    document.getElementById("throbber").className = "";
+    errorMessage.className = "Hidden";
+    throbber.className = "";
 });
 
 function attachClickEvents(anchors) {
